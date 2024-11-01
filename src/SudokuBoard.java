d071988e9ed3d1c5221f9426d25516e37b05a44c
LinaGSM <lina.gasmi@etu.univ-cotedazur.fr>
2024-11-01 16:43:04 +0100

    Implémentation de SudokuBoard

--- src/SudokuBoard.java
+++ src/SudokuBoard.java
@@ -1,2 +1,13 @@
+import java.util.HashMap;
+
 public class SudokuBoard {
+    private Cell[][] board;
+    private HashMap<Integer,Cell[]> allBlocks;
+
+
+
+//    private Integer getBlock() {
+//        switch () {
+//        }
+//    }
 }
