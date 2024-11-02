df844b993a798bfc856064dc09fe6572c1a83360
LinaGSM <lina.gasmi@etu.univ-cotedazur.fr>
2024-11-02 17:04:08 +0100

    Implémentation de SudokuBoard et de Cell + Lecture du fichier directement dans le constructeur de SudokuBoard

--- src/Cell.java
+++ src/Cell.java
@@ -1,16 +1,17 @@
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Cell {
     public int realValue;
     public List<Integer> possibleValue = List.of(1,2,3,4,5,6,7,8,9);
-    public Integer block;               //  à initialiser
 
-    public Cell(int realValue,Integer block) {
+    public Cell(int realValue) {
         this.realValue = realValue;
-        possibleValue = new ArrayList<>();
+        //possibleValue = new ArrayList<>();
+
         if (realValue != 0) {
             possibleValue = List.of(realValue);
         }
-        this.block = block;
+
     }
 }
