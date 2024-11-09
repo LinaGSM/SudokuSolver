de5633b7bd4ccf3ca4f37753d98c12f95cf86c87
captainkomic <magiqueram@gmail.com>
2024-11-09 09:26:33 +0100

    Ajout de nombre de ligne , colonne et bloque dans la classe Cell

--- src/Cell.java
+++ src/Cell.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 public class Cell {
     public int realValue;
     public List<Integer> possibleValue = List.of(1,2,3,4,5,6,7,8,9);
+    public int rowNumber;
+    public int columnNumber;
+    public int blockNumber;
 
     public Cell(int realValue) {
         this.realValue = realValue;
