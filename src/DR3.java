d87f0e954136da32c693b24c671869144fd9d1c5
captainkomic <magiqueram@gmail.com>
2024-11-10 15:26:21 +0100

    Ajout d'une méthode pour obtenir une liste de cellule avec exactement 2 candidats

--- src/DR3.java
+++ src/DR3.java
@@ -2,4 +2,19 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class DR3 extends DeductionRule{
+    //identification de toute les cellules ayant exactement 2 possibilités
+    ArrayList<Cell> findCellsWithTwoCandidates(SudokuBoard board) {
+        ArrayList<Cell> listOfCellWithTwoCandidates = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            CollectionIterator rowIterator = board.createIterator(IteratorType.ROW, i);
+            while (rowIterator.hasNext()) {
+                Cell currentCell = rowIterator.next();
+                if ((currentCell.realValue == 0) && (currentCell.possibleValue.size() == 2)) {
+                    listOfCellWithTwoCandidates.add(currentCell); // Ajoute la cellule
+                }
+            }
+        }
+        return listOfCellWithTwoCandidates;
+    }
+
 }
