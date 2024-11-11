f1ede3a055474a811c8145840b7bd7f7319da7e3
captainkomic <magiqueram@gmail.com>
2024-11-11 09:16:24 +0100

    Suppression de commentaires

--- src/RowIterator.java
+++ src/RowIterator.java
@@ -1,24 +1,3 @@
-/*
-public class RowIterable extends CollectionIterator {
-    public Cell[] row;
-// A MODIFIER
-    // es-ce qu'il faut un "premier"
-    // Vérifier que c'est pas pareil partout pour next() et hasNext()
-
-    public RowIterable(Cell[][] board, int numRow) {
-        row = board[numRow];
-    }
-
-    public Cell next(Cell cell) {
-        index++; // A VERIFIER
-        return row[index];
-    }
-
-    public boolean hasNext(Cell cell) {
-        return index < row.length;
-    }
-
-}*/
 public class RowIterator implements CollectionIterator{
     private Cell[][] grille;
     private int rowNb;  // le numéro de la ligne
@@ -47,8 +26,6 @@ public class RowIterator implements CollectionIterator{
     @Override
     public Cell next() {
         if(hasNext()){
-//            int i = prochainIndice;
-//            prochainIndice++;
             return grille[rowNb][prochainIndice++];
         }
         return grille[rowNb][prochainIndice]; //trouver quoi retourner comme Cell
