903593b9f4042c8e2db047bdef12c449abb77bb1
captainkomic <magiqueram@gmail.com>
2024-11-11 09:20:22 +0100

    Suppression de commentaires , de prints et une methode obsolètes

--- src/Cell.java
+++ src/Cell.java
@@ -1,14 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 
-/* Un Cellule possède:
-
-une valeur qui est la valeur actuelle (ou vrai valeur) de la case : realValue
-une liste de valeurs qui sont les potentielles valeurs que peut avoir la case : possibleValue
-  (elle est de taille 1 et comporte realValue si on a trouvé la vraie valeur de la case)
-
-La Cellule appartient à une ligne, à une colonne et à un bloc*/
-
 public class Cell {
     public int realValue;   //valeur actuelle
     public List<Integer> possibleValue = new ArrayList<>(List.of(1,2,3,4,5,6,7,8,9));    // liste de potentielles valeurs
@@ -34,9 +26,7 @@ public class Cell {
 
     public void removeFromPossibleValue(int value) {
         if(!isValid() && (possibleValue.contains(value))) {
-            //System.out.println(possibleValue);
             possibleValue.remove(Integer.valueOf(value));
-            //System.out.println("On a retire de la cellule "+rowNumber+":"+columnNumber+"  :   " + value);
         }
         verifyNoMorePossibilities();
     }
@@ -67,24 +57,4 @@ public class Cell {
         return cell != null && isInSameRow(cell) && isInSameColumn(cell);
     }
 
-
-//isItersectionOf renvoit true si la cellule est dans l'intersection entre le bloc numBloc et l'area/type (ligne ou colonne) numType
-    //  -intersection d'un bloc et d'une ligne
-    //  ou
-    //  -intersection d'un bloc et d'une colonne
-
-    public boolean isIntersectionOf(int numBloc, IteratorType type, int numType ) {
-        // Pas besoin de regarder l'intersection entre une ligne et une colonne: ce sera une unique cellule
-        boolean inSameArea;
-
-        switch (type) {
-            case ROW:
-                inSameArea = numType == this.rowNumber;
-            case COLUMN:
-                inSameArea = numType == this.columnNumber;
-            default:
-                inSameArea = false;
-        }
-        return (this.blockNumber==numBloc && inSameArea);
-    }
 }
